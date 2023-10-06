dfe14ace585853857222951a96601945c448cc76
Bac <ngqbac@gmail.com>
2023-10-06 23:25:10 +0700

    test

--- Runtime/DebugXController.cs
+++ Runtime/DebugXController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class DebugXController : MonoBehaviour
 {
     public static DebugXController Instance;
+    public bool testBool;
+    [ConditionalField(nameof(testBool))]public int testInt;
 
     private void Awake()
     {
