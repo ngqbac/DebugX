d3c470b680a3cee9969fb9c5ab90e26bf4be6f31
Bac <ngqbac@gmail.com>
2024-02-10 16:45:25 +0700

    Update SingletonPersistent.cs

--- Singleton/SingletonPersistent.cs
+++ Singleton/SingletonPersistent.cs
@@ -2,17 +2,17 @@ using UnityEngine;
 
 namespace DebugX.Singleton
 {
-    public class SingletonPersistent<T> : MonoBehaviour
-        where T : Component
+    public class SingletonPersistent<T> : MonoBehaviour where T : Component
     {
         public static T Instance { get; private set; }
-        
+
         public virtual void Awake()
         {
             if (Instance == null)
             {
                 Instance = this as T;
-                name = typeof(T).Name;
+
+                name = typeof(T).FullName ?? typeof(T).Name;
                 DontDestroyOnLoad(this);
             }
             else
