bf79ecd2f21e501b602cc85b804b8dcdabe4af89
Bac <ngqbac@gmail.com>
2024-02-26 21:47:30 +0700

    Update 1.3.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,20 +1,23 @@
-## [Version 1.0.2] - Date (2023-10-06)
-- Added feature LogUtility.
-- Added MyBox
+## <span style="color:orange">[Version 1.3.3]</span> - 2024-02-26
+- <b><span style="color:green">Added:</span></b> RendererExtensions
 
-## [Version 1.0.3] - Date (2023-10-08)
-- Added feature Console.
+## <span style="color:orange">[Version 1.3.2]</span> - 2024-02-25
+- <b><span style="color:red">Hotfix</span></b> UnityEditor reference
 
-## [Version 1.1.0] - Date (2023-11-25)
-- Modified class name.
-- Added InternalUtility.
+## <span style="color:orange">[Version 1.3.1]</span> - 2024-02-12
+- <b><span style="color:red">Removed:</span></b> MyBox
+- <b><span style="color:red">Removed:</span></b> Console (under development)
+- <b><span style="color:green">Added:</span></b> ConditionalField
+- <b><span style="color:green">Added:</span></b> LogFormat (Color/Affix)
+- <b><span style="color:gray">Re-structure plugin</span></b>
 
-## [Version 1.3.1] - Date (2024-02-12)
-- Remove MyBox
-- Remove Console feature (under development)
-- Re-structure
-- Add ConditionalField
-- Add LogFormat: Color/Affix
+## <span style="color:orange">[Version 1.1.0]</span> - 2023-11-25
+- <b><span style="color:gray">Modified class name</span></b>
+- <b><span style="color:green">Added:</span></b> InternalUtility.
 
-## [Version 1.3.2] - Date (2024-02-25)
-- Hotfix UnityEditor reference
+## <span style="color:orange">[Version 1.0.3]</span> - 2023-10-08
+- <b><span style="color:green">Added:</span></b> Console
+
+## <span style="color:orange">[Version 1.0.2]</span> - 2023-10-06
+- <b><span style="color:green">Added:</span></b> LogUtility
+- <b><span style="color:green">Added:</span></b> MyBox
---
+++ Extensions/RendererExtensions.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace DebugX.Extensions
+{
+    public static class RendererExtensions
+    {
+        public static void AddMaterial(this Renderer meshRenderer, Material material)
+        {
+            var currentMats = meshRenderer.sharedMaterials;
+            if (currentMats.Contains(material)) return;
+            var newMats = new List<Material>(currentMats) { material };
+            newMats[currentMats.Length] = material;
+            meshRenderer.sharedMaterials = newMats.ToArray();
+        }
+
+        public static void ChangeMaterial(this Renderer meshRenderer, Material materialRoot, Material materialTarget)
+        {
+            var currentMats = meshRenderer.sharedMaterials;
+
+            if (currentMats.Contains(materialRoot))
+            {
+                var index = System.Array.IndexOf(currentMats, materialRoot);
+                var newMats = new List<Material>(currentMats)
+                {
+                    [index] = materialTarget
+                };
+                meshRenderer.sharedMaterials = newMats.ToArray();
+            }
+            else
+            {
+                AddMaterial(meshRenderer, materialTarget);
+            }
+        }
+
+        public static void RemoveMaterial(this Renderer meshRenderer, Material material)
+        {
+            var currentMats = meshRenderer.sharedMaterials;
+            if (!currentMats.Contains(material)) return;
+            var index = System.Array.IndexOf(currentMats, material);
+            var newMats = currentMats.Where((mat, i) => i != index).ToArray();
+            meshRenderer.sharedMaterials = newMats;
+        }
+
+    }
+}
